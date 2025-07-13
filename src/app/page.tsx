a4c4b579a1974e2b0a9e6e7a435195471f156f65
Breno Jeronimo <brenojsm@outlook.com>
2025-07-13 17:37:05 -0300

    chore: update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import type { Metadata, Viewport } from "next"
-import HomePage from "@/pages/home-page";
+import React from "react";
+import type {Metadata, Viewport} from "next"
+
+import {getLandingPageData} from "@/lib/data";
 import {LandingPageData} from "@/types/app";
+import HeroSection from "@/components/sections/hero-section";
+import ServicesSection from "@/components/sections/services-section";
+import DifferentialsSection from "@/components/sections/differentials-section";
+import AboutSection from "@/components/sections/about-section";
+import FaqSection from "@/components/sections/faq-section";
+import TestimonialsSection from "@/components/sections/testimonials-section";
+import LocalizationSection from "@/components/sections/localization-section";
+import WhatsappButton from "@/components/whatsapp-button";
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -14,9 +24,26 @@ export const metadata: Metadata = {
     "Igor Santos Advocacia | Advocacia Especialista em Ações Bancárias.  Trabalhamos com compromisso e excelência para defender você da melhor forma. Entre em contato e veja como podemos proteger seus interesses.",
 }
 
-export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data/landing.json`);
-  const data: LandingPageData = await res.json()
+export default async function HomePage() {
+  const data: LandingPageData = await getLandingPageData()
 
-  return <HomePage data={data} />
+  return (
+    <main className="overflow-hidden ">
+      <HeroSection data={data.hero}/>
+
+      <ServicesSection data={data.services}/>
+
+      <DifferentialsSection data={data.differentials}/>
+
+      <AboutSection data={data.about}/>
+
+      <FaqSection data={data.faq}/>
+
+      <TestimonialsSection data={data.testimonials}/>
+
+      <LocalizationSection data={data.localization}/>
+
+      <WhatsappButton data={data.whatsappButton}/>
+    </main>
+  )
 }
